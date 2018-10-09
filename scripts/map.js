036544d97b4091cf5589456f3534fa821cda1c11
Witold Olszyk <witoldolszyk@gmail.com>
2018-10-09 23:26:11 +0200

    add

---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
